3aef61aaa54724a813205cc7bfb6828450266468
Aiden Cullo <aidencullo@gmail.com>
2023-07-17 22:18:24 -0300

    function parameters lesson

---
+++ python/functions/functions2.py
@@ -0,0 +1,26 @@
+# we can also pass information (we call these parameters) to a function, let's try
+
+def print_parameter(parameter):
+    print(parameter)
+
+print_parameter(1)
+print_parameter("dog")
+
+# run this file and observe output
+
+# okay how about something more useful
+
+def sayHello(name):
+    print("hello " + name)
+
+# call the function and pass your name
+
+# we can pass multiple parameters to a function
+
+def add(a, b):
+    print(a + b)
+
+# exercises
+
+# write a function to print the multiplication (use *, ie 1 * 2 = 2) of two numbers
+# write a function to print the division (2 / 2 = 1) of two numbers
